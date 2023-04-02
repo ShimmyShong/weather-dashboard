8abc43e9977ecc6f35164b9d84512c576a01fe1d
unknown <lattabun@gmail.com>
2023-04-01 21:16:27 -0400

    added important API information to array

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,16 @@ var APIKey = 'd61bd0582ac1a1d03a90bc18af5c2b35';
 var city = 'nashville';
 var queryGeoURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
+var weatherInfoArray = []
+var newInfoObject = []
+
 var queryWeatherURL;
 var lat;
 var lon;
+var temp;
+var sky;
+var humidity;
+var windSpeed;
 
 function getCoordinates(){
     $.ajax({
@@ -24,12 +31,38 @@ function getWeatherDays(){
     $.ajax({
         url: queryWeatherURL,
         success: function(result){
-            console.log(result)
-            console.log(result.list[0].main.temp) // the temp is measures in kelvin
-            console.log(result.list[0].weather[0].main) // displays how the sky will look
+            for(e = 0; e < result.list.length; e++){
+                temp = result.list[e].main.temp; // the temp is measures in kelvin
+                sky = result.list[e].weather[0].main; // displays how the sky will look
+                humidity = result.list[e].main.humidity; // displays humidity levels
+                windSpeed = result.list[e].wind.speed; // displays wind speed in meters/sec
+                date = result.list[e].dt; // displays the date and time
+                getWeatherObject();
+            }
+            console.log(weatherInfoArray)
         }
     })
 }
 
+function getWeatherObject(){
+    temp = ((temp - 273.15) * (9/5) + 32).toFixed(2);
+    humidity = humidity + '%';
+    windSpeed = (windSpeed * 2.237).toFixed(2);
+    date = dayjs(date * 1000).format('MM/DD/YYYY')
+
+    var newInfoObject= {
+        temp: temp + ' °F',
+        sky: sky,
+        humidity: humidity,
+        windSpeed: windSpeed + ' MPH',
+        date: date
+    }
+    if(newInfoObject)
+    weatherInfoArray.push(newInfoObject)
+}
+
+function addListElements(){
+
+}
 
 getCoordinates();
