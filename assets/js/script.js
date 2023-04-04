4c83e5ffe19070f3b1faa803637c1decf82b6be6
unknown <lattabun@gmail.com>
2023-04-04 01:14:23 -0400

    added past city searches, tidied html and css up

--- assets/js/script.js
+++ assets/js/script.js
@@ -84,7 +84,6 @@ function addCardElements(){ // adds card elements and classes depending on which
     cardEl4.addClass('card-text d-flex flex-column align-items-center m-1')
     titleEl.text(`${weatherInfoArray[e].date}`);
     cardEl1.text(`${weatherInfoArray[e].temp}`);
-    // cardEl2.text(`${weatherInfoArray[e].sky}`);
     cardEl3.text(`${weatherInfoArray[e].humidity}`);
     cardEl4.text(`${weatherInfoArray[e].windSpeed}`);
     if (!usedDays.includes(weatherInfoArray[e].date)){ // this if statement filters out days that were already appended before
@@ -102,14 +101,41 @@ function addCardElements(){ // adds card elements and classes depending on which
     // console.log(divEl.text())
 }
 
-buttonEl.on('click', function(event){ // TODO add icons and local storage
+buttonEl.on('click', function(event){
     event.preventDefault();
     usedDays = []; // need to reset the arrays on each click so that they can be used again in the functions
     weatherInfoArray = [];
-    $('#cards-container').empty(); // similarly to the arrays' the elements in the card container id needed to be emptied
-    city = $('#city-input').val();
+    $('#cards-container').empty(); // similarly to the arrays, the elements in the card container id needed to be emptied
+    city = $('#city-input').val().toLowerCase()// this forces the user's input to have the same format to reduce duplicates
     console.log(city)
     queryGeoURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey; // needed to redefine the URL in this eventlistener because it wouldnt work properly otherwise for reasons i dont understand
     console.log(queryGeoURL)
+
+    var previousCities = JSON.parse(localStorage.getItem('previousCities'))  // gets previous cities from local storage
+
+    if (!previousCities.includes(city)) { // adds the current city to the list of previous cities if it doesn't already exist
+        previousCities.push(city);
+        localStorage.setItem('previousCities', JSON.stringify(previousCities));
+    }
+
+    queryGeoURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
     getCoordinates();
 })
+
+var previousCities = JSON.parse(localStorage.getItem('previousCities')); // this gets the previously searched cities from local storage and adds them in the sidebar
+var sidebarEl = $('#sidebar');
+previousCities.forEach(function(city) {
+    var cityEl = $('<li>').addClass('list-group-item position-relative m-0').text(city);
+    sidebarEl.append(cityEl);
+
+    // Add a click event listener to the cityEl element
+    cityEl.on('click', function(event) {
+        event.preventDefault();
+        usedDays = [];
+        weatherInfoArray = [];
+        $('#cards-container').empty();
+        city = $(this).text().trim(); // get the city name from the clicked element
+        queryGeoURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+        getCoordinates();
+    });
+});
